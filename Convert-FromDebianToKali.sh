244adfcb2ffa630aa01dd4211fb723eefe885fae
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-04 12:01:12 -0500

    Add [Test-]Convert-FromDebianToKali.sh
    Tested on Debian 13 (aka. Trixie)

---
+++ Convert-FromDebianToKali.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Copyright 2026 dah4k
+# SPDX-License-Identifier: EPL-2.0
+
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get update  --quiet=2 --assume-yes --no-install-recommends
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assume-yes --no-install-recommends ca-certificates curl dirmngr gnupg
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC curl https://archive.kali.org/archive-keyring.gpg --output /usr/share/keyrings/kali-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/kali-archive-keyring.gpg] https://http.kali.org/kali kali-rolling main non-free contrib' | sudo tee --append /etc/apt/sources.list
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get update  --quiet=2 --assume-yes --no-install-recommends
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get upgrade --quiet=2 --assume-yes --no-install-recommends
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assume-yes --no-install-recommends kali-defaults kali-linux-headless
---
+++ Test-Convert-FromDebianToKali.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Copyright 2026 dah4k
+# SPDX-License-Identifier: EPL-2.0
+
+docker run --interactive --tty --rm -v ./Convert-FromDebianToKali.sh:/Convert-FromDebianToKali.sh debian:13
