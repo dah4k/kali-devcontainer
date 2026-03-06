35dfe96d5263be13adfd7fc373fc11fe6e9d36a4
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-05 19:59:35 -0500

    Switch from gnupg to gnupg2 package

--- Convert-FromDebianToKali.sh
+++ Convert-FromDebianToKali.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: EPL-2.0
 
 sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get update  --quiet=2 --assume-yes --no-install-recommends
-sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assume-yes --no-install-recommends ca-certificates curl dirmngr gnupg
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assume-yes --no-install-recommends ca-certificates curl dirmngr gnupg2
 sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC curl https://archive.kali.org/archive-keyring.gpg --output /usr/share/keyrings/kali-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/kali-archive-keyring.gpg] https://http.kali.org/kali kali-rolling main non-free contrib' | sudo tee --append /etc/apt/sources.list
 sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get update  --quiet=2 --assume-yes --no-install-recommends
