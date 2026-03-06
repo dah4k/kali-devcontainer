e727be4f4905b31cd699455f40de51001f650918
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-05 19:02:15 -0500

    Fix Test-Convert-FromDebianToKali.sh volume mount option

--- Test-Convert-FromDebianToKali.sh
+++ Test-Convert-FromDebianToKali.sh
@@ -2,4 +2,4 @@
 # Copyright 2026 dah4k
 # SPDX-License-Identifier: EPL-2.0
 
-docker run --interactive --tty --rm -v ./Convert-FromDebianToKali.sh:/Convert-FromDebianToKali.sh debian:13
+docker run --interactive --tty --rm -v ./Convert-FromDebianToKali.sh:/Convert-FromDebianToKali.sh:Z debian:13
