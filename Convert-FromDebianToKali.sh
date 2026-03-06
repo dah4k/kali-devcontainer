bc258ec782d7d6045a30a4487cefcefa33c49bb8
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-06 18:16:53 -0500

    Better use apt-get full-upgrade, not apt-get upgrade...
    See Also: https://www.kali.org/docs/troubleshooting/handling-common-apt-errors

--- Convert-FromDebianToKali.sh
+++ Convert-FromDebianToKali.sh
@@ -7,5 +7,5 @@ sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assum
 sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC curl https://archive.kali.org/archive-keyring.gpg --output /usr/share/keyrings/kali-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/kali-archive-keyring.gpg] https://http.kali.org/kali kali-rolling main non-free contrib' | sudo tee --append /etc/apt/sources.list
 sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get update  --quiet=2 --assume-yes --no-install-recommends
-sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get upgrade --quiet=2 --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get full-upgrade --quiet=2 --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" --fix-broken kali-defaults kali-linux-headless
