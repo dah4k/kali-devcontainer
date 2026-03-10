fb653ee37654359a0253f491cfdd9095e068db5a
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-09 21:47:58 -0400

    Install dbus-x11 package
    See Also: https://github.com/danchitnis/LLrdc/blob/main/Dockerfile#L45

--- Enable-Xrdp.sh
+++ Enable-Xrdp.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: EPL-2.0
 
 sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get update  --quiet=2 --assume-yes --no-install-recommends
-sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assume-yes --no-install-recommends lightdm xrdp xfce4
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install --quiet=2 --assume-yes --no-install-recommends lightdm xrdp xfce4 dbus-x11
 
 sudo adduser xrdp ssl-cert
 echo "export XAUTHORITY=~/.Xauthority" >> ~/.profile
